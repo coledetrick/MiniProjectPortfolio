7f13d2ed8d9af606e2b6db2a0b29fba6d4e5c2a8
Cole Detrick <153991478+coledetrick@users.noreply.github.com>
2025-12-28 11:12:34 -0600

    Update analyze_Nginx.sh

--- analyze_Nginx/analyze_Nginx.sh
+++ analyze_Nginx/analyze_Nginx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #https://gist.githubusercontent.com/kamranahmedse/e66c3b9ea89a1a030d3b739eeeef22d0/raw/77fb3ac837a73c4f0206e78a236d885590b7ae35/nginx-access.log
-# https://www.digitalocean.com/community/tutorials/awk-command-linux-unix#printing-all-lines-in-a-file
+#https://www.digitalocean.com/community/tutorials/awk-command-linux-unix#printing-all-lines-in-a-file
 
 # top 5 ip addresses.
 awk '{print $1}' nginx.log | sort | uniq -c | sort -nr | head -5
